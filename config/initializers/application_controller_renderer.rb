0801cd083947fd5f0d5b32182c66dbc3ec761407
itchiness386 <itchiness386@gmail.com>
2020-01-15 14:52:10 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'dfd7e7e6b55497f9810242e94ca5be13599266ac28c843bb4a90caffe77045301a21206168c8f152e8d622d8bbcc48045ab116522fac3cee2282b94d7ceda549'
